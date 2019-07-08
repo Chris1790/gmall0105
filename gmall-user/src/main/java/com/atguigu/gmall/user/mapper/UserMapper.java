d1298f24c9fdd7aa7c5896065a554c84d899451b
Chris1790 <930006945@qq.com>
2019-07-08 18:33:55 +0800

    提交测试

---
+++ gmall-user/src/main/java/com/atguigu/gmall/user/mapper/UserMapper.java
@@ -0,0 +1,10 @@
+package com.atguigu.gmall.user.mapper;
+
+import com.atguigu.gmall.user.service.UserService;
+import org.springframework.stereotype.Controller;
+
+
+public interface UserMapper {
+
+
+}
